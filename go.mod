b193c62f33accdbedf08d408e2f7c9f4e5c95420
Sherling Rodriguez Estrella <sherlinger4@gmail.com>
2023-09-25 21:29:48 -0400

    Graceful shutdown complete

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/sherlingre/orders-api
 
 go 1.21.1
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/cespare/xxhash/v2 v2.2.0
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f
+	github.com/redis/go-redis/v9 v9.2.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/sherlingre/orders-api/application"
-	// "net/http"
 )
 
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app", err)
 	}
